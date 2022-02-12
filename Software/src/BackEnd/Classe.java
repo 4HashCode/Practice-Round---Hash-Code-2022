31011225feccddad2d7a87a1402d99d2a6de41c8
Audrey-Teles <audreytelesds@gmail.com>
2022-02-12 01:11:52 -0300

    Classe

--- Software/src/BackEnd/Classe.java
+++ Software/src/BackEnd/Classe.java
@@ -4,10 +4,30 @@
  */
 package BackEnd;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author samuel
  */
 public class Classe {
-    
+
+    public static void leitor(String path) throws IOException {
+        BufferedReader buffRead = new BufferedReader(new FileReader(path));
+        String linha = "";
+        while (true) {
+            if (linha != null) {
+        
+                JOptionPane.showMessageDialog(null, linha);
+            } else {
+                
+                break;
+            }
+            linha = buffRead.readLine();
+        }
+    }
+
 }
